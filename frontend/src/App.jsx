d5d97ca7804877b98aaa86ef75936cc21c536a00
hhafzahh <2001231F@student.tp.edu.sg>
2025-07-21 00:37:28 +0800

    fix: update minor fix after merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,37 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useNavigate,
-} from "react-router-dom";
-import Booking from "../pages/Bookings";
+import { Routes, Route } from "react-router-dom";
+import Booking from "./pages/Booking";
+import Landing from "./pages/Landing";
 import "./App.css";
-
-function Home() {
-  const navigate = useNavigate();
-  return (
-    <>
-      <h1>Hotel Booking App</h1>
-      <button onClick={() => navigate("/bookings")}>View Bookings</button>
-    </>
-  );
-}
+import HotelRooms from "./pages/HotelRooms";
+import SearchResults from "./pages/SearchResults";
+import NavBar from "./components/NavBar";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import HotelDetails from "./pages/HotelDetails/HotelDetails";
+import Payment from "./pages/Payment";
+import Particulars from "./pages/Particulars";
+import ParticularsCheck from "./pages/ParticularsCheck";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/bookings" element={<Booking />} />
-      </Routes>
-    </Router>
+    <>
+      <div>
+        <NavBar />
+        <main className="main-content"></main>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/results" element={<SearchResults />} />
+          <Route path="/bookings" element={<Booking />} />
+          <Route path="/rooms" element={<HotelRooms />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/hotels/:hotelId" element={<HotelDetails />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="/booking-details" element={<Particulars />} />
+          <Route path="/confirm-booking" element={<ParticularsCheck />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
