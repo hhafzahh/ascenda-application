249e76fcde307e5bedd32cea86f0250eab98d4e6
yyyyii <yuyirou02@gmail.com>
2025-08-04 21:30:58 +0800

    added ref for protected backend api route

--- backend/userservice/userRoute.js
+++ backend/userservice/userRoute.js
@@ -13,5 +13,16 @@ router.get("/profile", authMiddleware, (req, res) => {
   res.json({ message: "Secure profile", user: req.user });
 });
 
+// const token = localStorage.getItem("token");
+
+// fetch("http://localhost:3004/api/profile", {
+//   method: "GET",
+//   headers: {
+//     "Authorization": `Bearer ${token}`
+//   }
+// })
+//   .then(res => res.json())
+//   .then(data => console.log(data))
+//   .catch(err => console.error("Unauthorized:", err));
 
 module.exports = router;
