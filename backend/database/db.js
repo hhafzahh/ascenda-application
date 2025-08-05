c67cd4005f031efb42d0f683d62713eecace6913
hhafzahh <2001231F@student.tp.edu.sg>
2025-08-05 15:26:53 +0800

    fix: update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -2,12 +2,11 @@
 const { MongoClient } = require("mongodb");
 require("dotenv").config({ path: "../.env" }); //need the path cuz both folders use the env var
 
-
 const connection_str = process.env.MONGODB_CONNECTIONSTR;
 const client = new MongoClient(connection_str);
 const dbName = "hotelbookingdb";
 
-const client = new MongoClient(connection_str);
+//const client = new MongoClient(connection_str);
 // let db = null;
 
 // async function connect() {
